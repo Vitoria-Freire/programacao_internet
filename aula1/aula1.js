0dfab54867048fcb0498f4cdcdcda2aac2c5cb31
Vitória Freire <vitoriafs200703@gmail.com>
2025-04-09 11:55:04 -0300

    Primeira aula de programacao para internet

---
+++ aula1/aula1.js
@@ -0,0 +1,12 @@
+// function message(){
+//     alert('Quer saber o teu valor? Olha para a cruz, tu vales um Deus crucificado')
+// }
+function mudou(){
+    var image = document.getElementById('img');
+    if (image.src = 'https://www.w3schools.com/js/pic_bulboff.gif'){
+        image.src = 'https://www.w3schools.com/js/pic_bulbon.gif';
+    }
+    else {
+        image.src = 'https://www.w3schools.com/js/pic_bulboff.gif'
+    }
+}
