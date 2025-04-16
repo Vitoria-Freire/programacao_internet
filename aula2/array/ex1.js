df82fd323c3aade25cf3e60d12898749cdbb20fd
Vitoria-Freire <vitoriafs200703@gmail.com>
2025-04-16 11:44:51 -0300

    Aprendendo o funcionamento do Array

---
+++ aula2/array/ex1.js
@@ -0,0 +1,9 @@
+dados_usuario = [];
+
+dados_usuario[0] = prompt('Qual o seu nome? ')
+dados_usuario[1] = prompt('Qual a sua cor preferida? ')
+dados_usuario[2] = prompt('Qual a sua comida preferida? ')
+dados_usuario[3] = prompt('Qual o seu estilo musical preferido? ')
+
+document.write('Olá ', dados_usuario[0], ' analisei suas preferências e estou vendo que sua cor preferida é ', dados_usuario[1], ' seu prato preferido é ', dados_usuario[2], ' e que você gosta de ouvir ', dados_usuario[3], ', agora que já sei de tudo isso, vou selecionar as melhores notícias para você ler hoje!');
+document.write('Olá ', dados_usuario[0], ' analisei suas preferências e estou vendo que sua cor preferida é ', dados_usuario[1], ' seu prato preferido é ', dados_usuario[2], ' e que você gosta de ouvir ', dados_usuario[3], ', agora que já sei de tudo isso, vou selecionar as melhores notícias para você ler hoje!');
---
+++ aula2/array/ex3.js
@@ -0,0 +1,15 @@
+entreternimento = [];
+
+entreternimento[0] = [];
+entreternimento[1] = [];
+
+entreternimento[0][0] = 'Game of Thrones';
+entreternimento[0][1] = 'The Big Bang Theory';
+entreternimento[0][2] = 'Friends';
+
+entreternimento[1][0] = 'Star Wars';
+entreternimento[1][1] = 'The Fast and the Furious';
+entreternimento[1][2] = 'The Lord of the Rings';
+
+document.write(entreternimento[0][1] + '<br\>'); 
+document.write(entreternimento[1][2]);
