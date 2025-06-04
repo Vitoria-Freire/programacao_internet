f2530c9ae712d5b6b5caeb8d9bda91d7e63dafaa
Vitoria-Freire <vitoriafs200703@gmail.com>
2025-06-04 11:16:43 -0300

    colocando um arquivo html como base
    Começando a reutilizar os templates html para diferentes paginas

--- flask/aula1/app/routes.py
+++ flask/aula1/app/routes.py
@@ -4,21 +4,26 @@ from flask import render_template
 
 @app.route('/')
 def hello():
-    user = 'Vitória'
-    return render_template('index.html', nome1 = user)
+    #user = 'Vitória'
+    return render_template('index.html')
 
-@app.route('/')
-def home():
-    usuario = { 
-        'nome' : 'Vitória',
-        'produtos' : ['Banana', 'Abacaxi', 'Melancia']
-    }
-    logado = True
-    return render_template('index.html', usuario = usuario, logado = logado)
+@app.route('/sobre')
+def sob():
+    #user = 'Vitória'
+    return render_template('sobre.html')
 
-@app.route('/endereco')
+# @app.route('/')
+# def home():
+#     usuario = { 
+#         'nome' : 'Vitória',
+#         'produtos' : ['Banana', 'Abacaxi', 'Melancia']
+#     }
+#     logado = True
+#     return render_template('index.html', usuario = usuario, logado = logado)
+
+@app.route('/contato')
 def hola():
-    return 'Moro em Ceará-Mirim/RN'
+    return render_template('contato.html')
 
 @app.route('/preferidos')
 def ola():
