3f879a8f041fc41d67cadac154e0d09a9495a869
Vitória Freire <vitoriafs200703@gmail.com>
2025-09-18 08:23:41 -0300

    adicionando o migrate

--- flask/aula1/app/__init__.py
+++ flask/aula1/app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_wtf import CSRFProtect
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 csrf = CSRFProtect(app)
+migrate = Migrate()
+db.init_app(app)
+migrate.init_app(app, db)
 
 from app import routes, models
 
