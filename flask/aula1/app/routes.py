b68013000e6b387c0d6fd4e2b3140a607900a2be
Vitória Freire <vitoriafs200703@gmail.com>
2025-09-25 11:19:53 -0300

    Tirando os comentarios + modificando rotas

--- flask/aula1/app/routes.py
+++ flask/aula1/app/routes.py
@@ -1,44 +1,3 @@
-# from app import app
-# from flask import render_template, redirect
-# from app.forms.login_form import LoginForm, UserForm
-# from app.controllers.authenticationController import AutheticationController
-# from app.models import Usuario
-# from app import db
-# import sqlalchemy as sa
-
-# @app.route('/')
-# def hello():
-#     return render_template('index.html', usuario = None, usuario_logado = False)
-
-# @app.route('/sobre')
-# def sob():
-#     return render_template('sobre.html')
-
-# @app.route('/contato')
-# def hola():
-#     return render_template('contato.html')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     formulario = LoginForm()
-#     if formulario.validate_on_submit():
-#         return AutheticationController.login(formulario)
-#     return render_template('login.html', title='Login', form=formulario)
-    
-# @app.route('/inserir', methods=['GET', 'POST'])
-# def inserindo():
-#     formulario = UserForm()
-#     if formulario.validate_on_submit():
-#         usuario = Usuario (
-#             username = formulario.username.data,
-#             email = formulario.email.data,
-#             password_hash =formulario.password.data
-#         )
-#         db.session.add(usuario)
-#         db.session.commit()
-#         return redirect('/')
-#     return render_template('login.html', title='Cadastro', form=formulario)
-
 from app import app
 from app import db
 from flask import render_template, flash, redirect, url_for
@@ -54,20 +13,21 @@ from app.forms.usuario_form import UsuarioForm
 
 @app.route("/")
 def home():
-    return render_template("index.html", usuario = None, usuario_logado = False)
-
-
-@app.route("/sobre")
-def sobre():
-    return "Página Sobre"
-
+    return render_template("index.html")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     formulario = LoginForm()
     if formulario.validate_on_submit():
-        return UsuarioController.login(formulario)
+        if AutheticationController.login(formulario):
+            flash ('Login realizado com sucesso!', 'success')
+            return redirect(url_for('home'))
+        else: 
+            flash ('Usuário ou senha inválidos', 'error')
     return render_template('login.html', title='Login', form = formulario)
+
+@app.route('/logout')
+def logout():
 
 
 @app.route("/inserir", methods=['GET', 'POST'])
