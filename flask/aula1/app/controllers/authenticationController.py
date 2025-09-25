4c8100a84a3322ff6120affee8e14210a9fa3082
Vitória Freire <vitoriafs200703@gmail.com>
2025-09-25 11:23:22 -0300

    atualizando funcao de login

--- flask/aula1/app/controllers/authenticationController.py
+++ flask/aula1/app/controllers/authenticationController.py
@@ -1,10 +1,19 @@
 from flask import render_template, flash
+from app.models.usuario import Usuario
+from flask_login import login_user, logout_user
+from werkzeug.security import check_password_hash
 
 class AutheticationController:
 
-    def login(form):
-        flash(f'O usuario {form.username.data} fez o login, lembrar={form.remember_me.data}')
-        usuario_logado = {
-            'nome': form.username.data
-        }
-        return render_template('index.html', usuario = usuario_logado, usuario_logado = True)
+    def login(formulario):
+        username = formulario.username.data.strip()
+        user = Usuario.query.filter_by(username=username).first()
+
+        if user:
+            if check_password_hash(user.password_hash,formulario.password.data):
+                login_user(user, remember=formulario.remember_me.data)
+                return True
+            else:
+                return False
+            
+    def logout()
